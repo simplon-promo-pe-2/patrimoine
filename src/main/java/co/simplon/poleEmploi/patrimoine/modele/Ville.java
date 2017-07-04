b860084ca78bf8c8c9af4f7fa98c48f8fabcc230
Eric Siber <eric.siber@gmail.com>
2017-07-04 17:24:04 +0200

    Etapes 3 et 4

--- src/main/java/co/simplon/poleEmploi/patrimoine/modele/Ville.java
+++ src/main/java/co/simplon/poleEmploi/patrimoine/modele/Ville.java
@@ -1,8 +1,22 @@
 package co.simplon.poleEmploi.patrimoine.modele;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CITIES")
 public class Ville {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ID")
 	private Long id;
+
+	@Column(name = "NAME")
 	private String nom;
 
 	public Long getId() {
