cd3e403fc703f35396d5d51bec8cc0d21717cb09
Eric Siber <eric.siber@gmail.com>
2017-07-05 16:14:53 +0200

    Etape 8

--- src/main/java/co/simplon/poleEmploi/patrimoine/modele/Ville.java
+++ src/main/java/co/simplon/poleEmploi/patrimoine/modele/Ville.java
@@ -1,10 +1,14 @@
 package co.simplon.poleEmploi.patrimoine.modele;
 
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -25,6 +29,10 @@ public class Ville {
 	@Column(name = "LONGITUDE")
 	private double longitude;
 
+	@OneToMany
+	@JoinColumn(name="CITIES_ID")
+	private Set<Monument> monuments;
+	
 	public Long getId() {
 		return id;
 	}
@@ -47,5 +55,9 @@ public class Ville {
 
 	public double getLatitude() {
 		return this.latitude;
+	}
+	
+	public Set<Monument> getMonuments() {
+		return monuments;
 	}
 }
--- src/test/java/co/simplon/poleEmploi/patrimoine/VilleJpaTest.java
+++ src/test/java/co/simplon/poleEmploi/patrimoine/VilleJpaTest.java
@@ -13,14 +13,25 @@ import co.simplon.poleEmploi.patrimoine.modele.Ville;
 public class VilleJpaTest {
 
 	@Test
+	public void l_ensemble_des_monuments_de_Paris_est_accessible() {
+		// GIVEN
+		Long idRecherche = 3685L;
+		
+		// WHEN
+		EntityManager em = getEntityManager();
+		Ville paris = em.find(Ville.class, idRecherche);
+		
+		// THEN
+		assertEquals(4, paris.getMonuments().size());
+	}
+	
+	@Test
 	public void la_ville_d_identifiant_25925_doit_etre_Forstfeld() {
 		// GIVEN
 		Long idRecherche = 25925L;
 
 		// WHEN
-		EntityManagerFactory emf = Persistence
-				.createEntityManagerFactory("BasePatrimoine");
-		EntityManager em = emf.createEntityManager();
+		EntityManager em = getEntityManager();
 		Ville ville = em.find(Ville.class, idRecherche);
 
 		// THEN
@@ -28,5 +39,12 @@ public class VilleJpaTest {
 		assertEquals("Forstfeld", ville.getNom());
 		assertEquals(48.85, ville.getLatitude(), 0);
 		assertEquals(8.033333, ville.getLongitude(), 0);
+	}
+
+	private EntityManager getEntityManager() {
+		EntityManagerFactory emf = Persistence
+				.createEntityManagerFactory("BasePatrimoine");
+		EntityManager em = emf.createEntityManager();
+		return em;
 	}
 }
